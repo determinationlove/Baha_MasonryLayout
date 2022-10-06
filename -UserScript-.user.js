1b53eaf644383b2e8c7629e08647cf5eed8a21b4
determinationlove <47052855+determinationlove@users.noreply.github.com>
2022-10-07 00:57:45 +0800

    test

---
+++ -UserScript-.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         測試
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://forum.gamer.com.tw/B.php?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none\
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js
+// @require      file://D:\程式\Baha_MasonryLayout\-UserScript-.user.js(脚本的地址)
+// ==/UserScript==
+
+(function () {
+    'use strict';
+/*
+    let block = document.querySelectorAll('tr.b-list__row ');
+
+    let blockTitle = document.querySelectorAll('tr.b-list__row>td>a.b-list__main__title ');
+
+    for(let i = 0; i < blockTitle.length; i++){
+        blockTitle[i].style.setProperty("text-decoration", "line-through");
+    }
+
+    for(let i = 0; i < block.length; i++){
+        block[i].style.setProperty("margin-bottom", "100px");
+    }
+    */
+
+    alert("Hello World!");
+    
+
+})();
