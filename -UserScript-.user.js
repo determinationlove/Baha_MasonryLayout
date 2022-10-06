b0e82d0f962908fba90c9c0e92cc4a793fe22a6b
determinationlove <47052855+determinationlove@users.noreply.github.com>
2022-10-07 01:10:30 +0800

    test

--- -UserScript-.user.js
+++ -UserScript-.user.js
@@ -1,33 +1,25 @@
 // ==UserScript==
-// @name         測試
+// @name         test
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://forum.gamer.com.tw/B.php?*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        none\
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js
-// @require      file://D:\程式\Baha_MasonryLayout\-UserScript-.user.js(脚本的地址)
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
-/*
-    let block = document.querySelectorAll('tr.b-list__row ');
 
-    let blockTitle = document.querySelectorAll('tr.b-list__row>td>a.b-list__main__title ');
-
-    for(let i = 0; i < blockTitle.length; i++){
-        blockTitle[i].style.setProperty("text-decoration", "line-through");
-    }
+    let block = document.querySelectorAll('tr ');
 
     for(let i = 0; i < block.length; i++){
-        block[i].style.setProperty("margin-bottom", "100px");
+        block[i].style.cssText = `
+            display: flex; 
+            background-color: red;
+        `;
     }
-    */
-
-    alert("Hello World!");
-    
 
 })();
