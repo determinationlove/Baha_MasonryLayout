aaf9c0ba2e12dfb0c7243e91342fd6cce1621eb8
Lehcar <tweetyho1234@gmail.com>
2022-10-27 16:09:40 +0800

    內文爬蟲 + RWD

---
+++ src/components/ArticleBody.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'tailwindcss/tailwind.css';
+import test from '../../assets/icon.png';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+export type Props = {
+    code_Body: any;
+    //onfocus: (link: any) => void;
+};
+
+const ArticleBody = ({code_Body} : Props) => {
+
+
+
+
+		return (
+			<div className='w-96 h-96 absolute flex bg-slate-400'>
+
+			</div>
+		);
+	
+}
