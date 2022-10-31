be8f699bcd9e9782778c5fd358e1e4d127eda4a3
determinationlove <47052855+determinationlove@users.noreply.github.com>
2022-11-01 02:04:59 +0800

    主樓留言區

--- src/components/ArticleBody.tsx
+++ src/components/ArticleBody.tsx
@@ -6,6 +6,7 @@ import test from '../../assets/icon.png';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 import App from 'renderer/App';
+import ArticleReply from './ArticleReply';
 
 export type Props = {
     code_Body: any;
@@ -15,7 +16,7 @@ export type Props = {
 const ArticleBody = ({ code_Body, setOpen }: Props) => {
     const ref = useRef(null);
 
-    //console.log(code_Body.Body_content);
+    //console.log(code_Body.Reply);
 
     //文章區域外部點擊檢測
     function useOutsideCheck(ref: any) {
@@ -37,9 +38,9 @@ const ArticleBody = ({ code_Body, setOpen }: Props) => {
 
     useOutsideCheck(ref);
 
-    const Tohtml = code_Body.Body_content;
+    const Tohtml = code_Body?.Body_content;
 
-    const url = code_Body.Body_link;
+    const url = code_Body?.Body_link;
 
     return (
         <div className="absolute z-20 h-full w-full bg-black/70">
@@ -47,49 +48,52 @@ const ArticleBody = ({ code_Body, setOpen }: Props) => {
                 <div
                     ref={ref}
                     className=" max-w fixed flex h-content w-5/12 flex-col
-				 items-start justify-start overflow-hidden rounded-2xl bg-white desktop:p-10 p-5
-                 lg:w-8/12 desktop:w-8/12
+				 items-start justify-start overflow-hidden rounded-2xl bg-white p-5 lg:w-8/12
+                 desktop:w-8/12 desktop:p-10
 				"
                 >
-                    <div className="flex h-full w-full flex-row">
-                        <div className="desktop:mr-2 flex desktop:w-40 lg:w-40">
+                    <div className="flex h-full w-full flex-row  overflow-x-visible overflow-y-scroll">
+                        <div className="flex lg:w-40 desktop:mr-2 desktop:w-40">
                             <img
-                                src={code_Body.Body_card}
-                                className="flex h-0 w-0 desktop:h-gamercard desktop:w-gamercard lg:h-gamercard lg:w-gamercard
-                                desktop:outline desktop:border-2 lg:outline lg:border-2
-                                outline-cyan-500 rounded-md border-slate-400 outline-offset-2"
+                                src={code_Body?.Body_card}
+                                className="flex h-0 w-0 rounded-md border-slate-400 outline-offset-2 outline-cyan-500
+                                xl:h-gamercard xl:w-gamercard xl:border-2 xl:outline
+                                lg:h-gamercard lg:w-gamercard lg:border-2 lg:outline
+                                desktop:h-gamercard desktop:w-gamercard desktop:border-2 desktop:outline"
                             />
                         </div>
 
                         <div className="flex h-full w-full flex-col">
                             <div className="text-2xl ">
-                                {code_Body.Body_title}
+                                {code_Body?.Body_title}
                             </div>
                             <div className="flex w-full flex-col p-2 desktop:p-5">
                                 <div className="flex w-full flex-row items-center justify-between">
-                                    <div className="text-sm desktop:text-lg text-teal-700">
-                                        {code_Body.Body_author_name}／
-                                        {code_Body.Body_author_id}
+                                    <div className="text-sm text-teal-700 desktop:text-lg">
+                                        {code_Body?.Body_author_name}／
+                                        {code_Body?.Body_author_id}
                                     </div>
                                     <a
                                         href={url}
                                         target="_blank"
-                                        className="text-sm desktop:text-lg desktop:font-medium text-blue-500"
+                                        className="text-sm text-blue-500 desktop:text-lg desktop:font-medium"
                                     >
                                         查看來源
                                     </a>
                                 </div>
 
-                                <div className="mt-2 desktop:mt-5 flex w-full flex-row items-center justify-between text-gray-500">
-                                    <div className="text-sm desktop:text-lg">{code_Body.Body_date}</div>
+                                <div className="mt-2 flex w-full flex-row items-center justify-between text-gray-500 desktop:mt-5">
+                                    <div className="text-sm desktop:text-lg">
+                                        {code_Body?.Body_date}
+                                    </div>
                                     <div className="flex flex-row desktop:text-xl">
                                         推-
-                                        <div className="mr-2  desktop:mr-10 text-orange-500">
-                                            {code_Body.Body_gp}
+                                        <div className="mr-2  text-orange-500 desktop:mr-10">
+                                            {code_Body?.Body_gp}
                                         </div>
                                         噓-
                                         <div className="text-violet-500">
-                                            {code_Body.Body_bp}
+                                            {code_Body?.Body_bp}
                                         </div>
                                     </div>
                                 </div>
@@ -97,9 +101,17 @@ const ArticleBody = ({ code_Body, setOpen }: Props) => {
                             <hr className="my-3 w-full"></hr>
                             <div
                                 dangerouslySetInnerHTML={{ __html: Tohtml }}
-                                className="w-full overflow-x-visible overflow-y-scroll p-2 desktop:p-10 "
+                                className="w-full p-2 desktop:p-10 "
                             ></div>
                             <hr className="my-3 w-full"></hr>
+
+                            {code_Body?.Reply.map((id: any, index: any) => {
+                                return (
+                                    <ArticleReply
+                                        Body_Reply={id}
+                                    ></ArticleReply>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
--- src/components/articleBlock.tsx
+++ src/components/articleBlock.tsx
@@ -6,6 +6,7 @@ import test from '../../assets/icon.png';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 import ArticleBody from './ArticleBody';
+import { eq } from 'cheerio/lib/api/traversing';
 
 export type Props = {
     code: any;
@@ -23,23 +24,105 @@ const ArticleBlock = ({ code, setOpen, SetArticleDataFunction }: Props) => {
 
             // 先把來源不可用的attr修正
             for (let i = 0; i < list.length; i++) {
-                $("img").each(function(){ //將 <img> 的屬性新增 src
-                    var old_src = $(this).attr("data-src");
-                    $(this).attr("src", old_src);
-                    $(this).attr("class", 'max-w-full mb-2 max-h-fit');
+                $('img').each(function () {
+                    //將 <img> 的屬性新增 src
+                    var old_src = $(this).attr('data-src');
+                    $(this).attr('src', old_src);
+                    $(this).attr('class', 'max-w-full mb-2 max-h-fit');
                 });
 
-                $("iframe").each(function(){ //將 <iframe> 的屬性新增 src
-                    var old_src = $(this).attr("data-src");
-                    $(this).attr("src", old_src);
-                    $(this).attr("class", 'w-iframe h-iframe max-w-full max-h-full mb-2');
+                $('iframe').each(function () {
+                    //將 <iframe> 的屬性新增 src
+                    var old_src = $(this).attr('data-src');
+                    $(this).attr('src', old_src);
+                    $(this).attr(
+                        'class',
+                        'w-iframe h-iframe max-w-full max-h-full mb-2'
+                    );
                 });
 
-                $("div").find('.photoswipe-image').each(function(){ //將 <iframe> 的屬性新增 src 
-                    $(this).removeAttr('href');
-                });
+                $('div')
+                    .find('.photoswipe-image')
+                    .each(function () {
+                        //將 <iframe> 的屬性新增 src
+                        $(this).removeAttr('href');
+                    });
+
+                // ------ 留言區整理 ------
+                list.eq(i)
+                    .find('.c-reply__head')
+                    .filter('.nocontent')
+                    .each(function () {
+                        // 刪除查看所有留言（來源就不是預設給出所有資料，所以放棄顯示完整留言）
+                        $(this).remove();
+                    });
+
+                list.eq(i)
+                    .find('.material-icons')
+                    .each(function () {
+                        // 刪除頭像的雜物
+                        $(this).remove();
+                    });
+
+                list.eq(i)
+                    .find('.buttonbar')
+                    .each(function () {
+                        // 刪除留言回覆
+                        $(this).remove();
+                    });
+
+                list.eq(i)
+                    .find('.c-reply__editor')
+                    .each(function () {
+                        // 刪除訪客留言
+                        $(this).remove();
+                    });
+
+                /*
+                list.eq(i)
+                    .find('.c-reply__item div')
+                    .each(function () {
+                        // 每個留言橫向排列
+                        $(this).attr(
+                            'class',
+                            'flex flex-row items-start w-full'
+                        );
+                    });
+
+                // 留言者暱稱顏色
+                list.eq(i)
+                    .find('.reply-content__user')
+                    .each(function () {
+                        $(this).attr(
+                            'class',
+                            'text-teal-700 whitespace-nowrap'
+                        );
+                    });
+
+                // 留言時間
+                list.eq(i)
+                    .find('.c-reply__item div')
+                    .eq(2)
+                    .eq(0)
+                    .each(function () {
+                        $(this).attr(
+                            'class',
+                            'flex items-center justify-end text-gray-500'
+                        );
+                    });
+
+                list.eq(i)
+                    .find('.reply-content__footer')
+                    .each(function () {
+                        // 留言時間位置靠後
+                        $(this).attr(
+                            'class',
+                            'flex flex-row items-center justify-end'
+                        );
+                    });
+                */
             }
-            
+
             for (let i = 0; i < list.length; i++) {
                 if (
                     list
@@ -51,7 +134,6 @@ const ArticleBlock = ({ code, setOpen, SetArticleDataFunction }: Props) => {
                         .find('.c-post__header h1')
                         .hasClass('c-post__header__title is-except')
                 ) {
-
                     const Body_title = code.title;
 
                     const Body_sort = code.sort;
@@ -84,13 +166,12 @@ const ArticleBlock = ({ code, setOpen, SetArticleDataFunction }: Props) => {
 
                     const Body_bp = list.eq(i).find('.bp a').text();
 
+                    //------------ 主樓內容 ------------//
                     let Body_content: any = list
                         .eq(i)
                         .find('.c-article__content')
                         .html();
 
-                    //let $t = cheerio.load(Body_content);
-                    
                     if (Body_content.length == 0) {
                         console.log(
                             'Body_content.length == 0, 第一層判斷: 只有一行文字'
@@ -110,8 +191,72 @@ const ArticleBlock = ({ code, setOpen, SetArticleDataFunction }: Props) => {
 
                     const Body_link = code.link;
 
-                    const Body_card = list.eq(i).find('.c-user a img').attr('src');
-                    
+                    const Body_card = list
+                        .eq(i)
+                        .find('.c-user a img')
+                        .attr('src');
+
+                    //================= 留言區 =================//
+
+                    const Reply: any = [];
+                    // 這裡的 eq(0) 指的不是以原本的 DOM 上面排序的 eq(0)
+                    // 而是把所有的 $('.c-section').find('.c-post__footer.c-reply') 篩出來之後開始數的第一順位
+                    const Reply_list = $('.c-section')
+                        .find('.c-post__footer.c-reply')
+                        .eq(0);
+
+                    for (
+                        let i = 0;
+                        i < Reply_list.find('.c-reply__item').length;
+                        i++
+                    ) {
+                        $('a').each(function() { // Go through all links
+                            if ($(this).text() == "https://") { // Change text to whatever you want to compare
+                                // Do what you want with links having certain text
+                                $(this).attr('href', $(this).text())
+                            }
+                        });
+
+                        // 大頭貼
+                        const Reply_pic: any = Reply_list.find('.c-reply__item')
+                            .eq(i)
+                            .find('.reply-avatar.user--sm')
+                            .html();
+
+                        // 暱稱
+                        const Reply_name: any = Reply_list.find(
+                            '.c-reply__item'
+                        )
+                            .eq(i)
+                            .find('.reply-content__user')
+                            .html();
+
+                        // 內容
+                        const Reply_concent: any = Reply_list.find(
+                            '.c-reply__item'
+                        )
+                            .eq(i)
+                            .find('.comment_content')
+                            .html();
+
+                        // 留言時間
+                        const Reply_time: any = Reply_list.find(
+                            '.c-reply__item'
+                        )
+                            .eq(i)
+                            .find('.reply-content__footer')
+                            .html();
+
+                        Reply.push({
+                            Reply_pic,
+                            Reply_name,
+                            Reply_concent,
+                            Reply_time,
+                        });
+                    }
+
+                    console.log(Reply[0]?.Reply_pic, Reply_list.length);
+
                     ArticleBody_Data.push({
                         Body_title,
                         Body_sort,
@@ -124,12 +269,12 @@ const ArticleBlock = ({ code, setOpen, SetArticleDataFunction }: Props) => {
                         Body_content,
                         Body_link,
                         Body_card,
+                        Reply,
                     });
                 }
             }
 
             setBody(ArticleBody_Data);
-            //console.log(ArticleBody_Data[0], code.link);
         });
     }, []);
 
